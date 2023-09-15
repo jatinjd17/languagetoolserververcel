f5b3608c86448eec6576b7b2ef07b6f22eb802e0
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-09-15 13:41:22 +0530

    Update server.py

--- api/server.py
+++ api/server.py
@@ -4,4 +4,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World123!"
+    f = open("filee.txt", "r")
+    lines = int(f.read())
+    if(lines < 5):
+        print('true')
+    print(str(lines))
+    f.close
+    if(lines < 13):
+        print('nolinktrigger')
+        y = open('filee.txt','w+')
+        k = lines + 1
+        y.write(str(k))
+        y.close
+        return 'nolinktrigger'
+
+    elif(lines == 20):
+        print('trigger')
+        y = open('filee.txt','w+')
+        y.write(str(1))
+        y.close
+        return 'trigger'
+    else:
+        b = random.randrange(0,3)
+        if(b == 2):
+            print('trigger')
+            y = open('filee.txt','w+')
+            y.write(str(1))
+            y.close
+            return 'trigger'
+        else:
+            print('nolinktrigger')
+            y = open('filee.txt','w+')
+            k = lines + 1
+            y.write(str(k))
+            y.close
+            return 'nolinktrigger'
+    # return "Hello World123!"
