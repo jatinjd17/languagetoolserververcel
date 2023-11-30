b8fb832aaf41e2cb8dd0ba763fd317f67e92af6a
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-11-30 12:26:54 +0530

    Update server.py

--- api/server.py
+++ api/server.py
@@ -99,6 +99,8 @@ def createinstancee():
         staticip2 = neww['externalip2']
         staticip3 = neww['externalip3']
         staticip4 = neww['externalip4']
+        projectID = neww['projectname']
+        serviceaccount = neww['serviceaccount']
         print('staticip1 --> ',staticip1, 'staticip2 --> ',staticip2, 'staticip3 --> ',staticip3, 'staticip4 --> ',staticip4)
     #     examplee = {
     #     "staticip1": staticip1,
@@ -120,8 +122,8 @@ def createinstancee():
         # serviceaccount = '46009241767'
         # projectID = 'fourthproject-403206'
         # serviceaccount = '881354389191'
-        projectID = 'fifthproject-403206'
-        serviceaccount = '639344720889'
+        # projectID = 'fifthproject-403206'
+        # serviceaccount = '639344720889'
         # projectID = 'sixthproject-403206'
         # serviceaccount = '935058106745'
         # projectID = 'seventh-project-403509'
@@ -208,6 +210,8 @@ def deleteinstancee():
         neww = request.get_json()
 
         email = neww['ipindex']
+        projectID = neww['projectname']
+    
         print(email)
         zonesss = {'northamerica-northeast1-a':'162','northamerica-northeast2-a':'188','southamerica-east1-b':'158','southamerica-west1-a':'194','us-central1-c':'128','us-east1-b':'142','us-east4-c':'150','us-east5-a':'202','us-south1-a':'206','us-west1-c':'138','us-west2-a':'168','us-west3-a':'180','us-west4-a':'182','europe-central2-a':'186','europe-north1-a':'166','europe-southwest1-a':'204','europe-west1-b':'132','europe-west10-a':'214','europe-west12-a':'210','europe-west2-c':'154','europe-west3-c':'156','europe-west4-a':'164','europe-west6-a':'172','europe-west8-a':'198','europe-west9-a':'200','australia-southeast1-b':'152','australia-southeast2-a':'192'}
 
@@ -215,7 +219,7 @@ def deleteinstancee():
         # projectID = 'second-project-400407'
         # projectID = 'thirdproject-402906'
         # projectID = 'fourthproject-403206'
-        projectID = 'fifthproject-403206'
+        # projectID = 'fifthproject-403206'
         # projectID = 'sixthproject-403206'
         # projectID = 'seventh-project-403509'
         # projectID = 'eightproject-403519'
